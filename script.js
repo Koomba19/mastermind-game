13cef1df6c29e5cc5e630e013ab7ff23990f218c
Koomba19 <jpollock505@hotmail.com>
2025-03-27 04:56:04 -0400

    End of night of 3/26, JS started

---
+++ script.js
@@ -0,0 +1,13 @@
+// Constants and Variables
+let secretCode = [];
+let playerAttempt = [];
+const maxTurns = 10;
+let currentTurn = 0;
+let gameWon = false;
+// Dom Elements
+// Functions
+function startGame() {};
+function getPlayerAttempt() {};
+function checkAttempt() {};
+function displayAttempt() {};
+// Game Loop
