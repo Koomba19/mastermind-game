ab7dc199450e0ed09ea8bdd2e5c85a0da9002302
Koomba19 <jpollock505@hotmail.com>
2025-04-01 04:32:58 -0400

    made the secret code generator

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// Constants and Variables
-let secretCode = [];
-let playerAttempt = [];
-const maxTurns = 10;
-let currentTurn = 0;
-let gameWon = false;
-// Dom Elements
-// Functions
-function startGame() {};
-function getPlayerAttempt() {};
-function checkAttempt() {};
-function displayAttempt() {};
-// Game Loop
+class mastermind {
+    constructor() {
+        this.colors = ['black', 'white', 'green', 'yellow', 'red', 'blue'];
+        this.secretCode = this.generateSecretCode();
+    }
+
+    generateSecretCode() {
+        let code = [];
+        for (let i = 0; i < 4; i++) {
+            let randomColor = Math.floor(Math.random() * 6);
+            code.push(this.colors[randomColor]);
+        }
+        return code;
+    }
+}
+const game = new mastermind();
